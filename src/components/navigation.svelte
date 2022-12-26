6a938bb955557afb306599c5998e94035e035d8e
Christian Smith Mantas <casm202@gmail.com>
2022-12-26 16:42:00 +0000

    Create: navigation bar

---
+++ src/components/navigation.svelte
@@ -0,0 +1,36 @@
+<script></script>
+
+<style>
+	.nav {
+		position: sticky;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem 2rem;
+		top: 0;
+		height: 10vh;;
+
+		background: #fd5319;
+		color: #fff;
+	}
+	.logo {
+		font-size: 1.5rem;
+	}
+	ul {
+		display: flex;
+		list-style: none;		
+		gap: 1rem;
+	}
+	li {
+		cursor: pointer;
+	}
+</style>
+
+<div class="nav">
+	<p class="logo">Fraction Books</p>
+	<ul>
+		<li>home</li>
+		<li>about</li>
+		<li>github</li>
+	</ul>
+</div>
